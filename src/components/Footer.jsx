3bc6b61add7032115f7a9198d15eafee7bc4282b
Дембіцький Олександр <oleksandr.dembitskyi.pp.2023@lpnu.ua>
2026-02-17 21:16:16 +0200

    feat: add Header and Footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2026 Лабораторна робота №1. Всі права захищені.</p>
+    </footer>
+  );
+}
+
+export default Footer;
