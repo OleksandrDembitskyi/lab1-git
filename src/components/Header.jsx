b60207030e6792ac64ae82cd7258a9685f82167e
Дембіцький Олександр <oleksandr.dembitskyi.pp.2023@lpnu.ua>
2026-02-17 22:58:54 +0200

    docs: add comments to Header and Footer components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 import './Header.css';
 
+// Компонент Header відповідає за верхню частину сторінки
+// Містить назву проєкту та навігаційне меню
 function Header() {
   return (
     <header className="header">
+      {/* Заголовок сайту */}
       <h1>Git Lab Work</h1>
+      
+      {/* Навігаційне меню з трьома посиланнями */}
       <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+        <a href="/">Home</a>      {/* Головна сторінка */}
+        <a href="/about">About</a> {/* Сторінка "Про нас" */}
+        <a href="/contact">Contact</a> {/* Сторінка контактів */}
       </nav>
     </header>
   );
