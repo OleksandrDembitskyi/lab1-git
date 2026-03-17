c5ca7bd4608ebdb23a12e750a3e77e9bfcdf7dc2
Дембіцький Олександр <oleksandr.dembitskyi.pp.2023@lpnu.ua>
2026-03-17 23:53:47 +0200

    feat: add PostHog analytics, custom events, feature flag and production env

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import posthog from 'posthog-js'
+import { PostHogProvider } from '@posthog/react'
+
+posthog.init(
+  import.meta.env.VITE_PUBLIC_POSTHOG_KEY,
+  {
+    api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+    person_profiles: 'identified_only',
+  }
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PostHogProvider client={posthog}>
+      <App />
+    </PostHogProvider>
   </StrictMode>,
 )
+
+
